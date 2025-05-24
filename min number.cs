8015509d3e908de073f7c39b364d3dbfad3d0173
rutujajadhav00 <rutujajadhav413512@gmail.com>
2025-05-24 23:25:05 +0530

    max is find#

---
+++ min number.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstCod 
+{
+
+    internal class min_number
+    {
+         static void main(String[] args)
+        {
+            int[] larr1 = new int[5];
+            Console.WriteLine("enter value for 5 integer");
+
+            for (int i = 0; i < larr1.Length; i++)
+            {
+                larr1[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int mini = larr1 [0];
+            for (int i = 0; i < 5; i++)
+            {
+                if (mini > larr1[i])
+                {
+                    mini = larr1[i];
+                }
+            }
+            Console.WriteLine("THE mini value :" + mini);
+        }
+
+
+}   }
